36ae5e561018a6ac4b627eefd407f98c082e0a4b
Jewel Carrier-Davis <jujicb@gmail.com>
2021-02-25 04:04:01 -0500

    commit tree c4a4950095c1ba69343957f7905eee00b0b2d934

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+case array.length
+when 1
+    array[0]
+when 2
+    array.join(" and ")
+when 3
+    array[2] = "and #{array[2]}"
+    array.join(", ")
+else
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+end
 end
